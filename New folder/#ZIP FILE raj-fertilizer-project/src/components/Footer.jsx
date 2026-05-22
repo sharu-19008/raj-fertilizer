ac6ad2cf4bc3e0d86e5d9e48e904e95d0bacddc9
sharu <rshar7854@gmail.com>
2026-05-22 11:57:29 +0530

    removed new folder

--- New folder/#ZIP FILE raj-fertilizer-project/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return(
-        <footer className="bg-gray-800 text-white flex justify-center p-3">
-            <p>
-                &copy;2025 Developed by Sharvani R
-            </p>
-        </footer>
-    ) 
-}
